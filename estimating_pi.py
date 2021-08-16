7455729238b79afe3ba667eb902a71dc257d30c5
thirdratecyberpunk <blackbul@aston.ac.uk>
2021-08-16 17:49:32 +0100

    Added animation of plotting process

--- estimating_pi.py
+++ estimating_pi.py
@@ -7,17 +7,48 @@ Pi can be calculated as 4 * # (points in circle / # points)
 
 # TODO: add visualisation to this process using matplotlib
 
-import random, math
+import random
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots()
+ax.set_xlim(-1, 1)
+ax.set_ylim(-1, 1)
+ln, = plt.plot([], [], 'ro')
+x_data = []
+y_data = []
+
+def update(frame):
+    x_data.append(frame[0])
+    y_data.append(frame[1])
+    ln.set_data(x_data, y_data)
+    return ln,
 
 def main():
+    """
+    Main function that generates the visualisation
+    """
     random.seed(1)
     points = 1000000
     print(f"Estimating pi with {points} points")
 
+    # plotting circle for reference
+    # angle = np.linspace(0 , 2 * np.pi , 150)
+    # radius = 1
+    # circle_x = radius * np.cos(angle)
+    # circle_y = radius * np.sin(angle)
+
+    # generating random points and plotting them
     points_in_circle = 0
+    values = []
 
     for i in range(points):
-        x, y = generate_random_point()
+        value = generate_random_point()
+        values.append(value)
+        x = value[0]
+        y = value[1]
         if (x*x + y*y < 1):
             points_in_circle += 1
 
@@ -26,13 +57,18 @@ def main():
 
     print(f"Calculated pi as {pi_approximation} with an accuracy of {accuracy_pct}%")
 
+    # plotting results onto animated graph
+    ani = FuncAnimation(fig, update, frames=values, interval=1, blit=True)
+    # ani.save("estimating_pi.gif")
+    plt.show()
+
 def generate_random_point():
     """
-    Returns a pair of random values
+    Returns a tuple of random values
     """
     x = 2.0 * random.random() - 1.0
     y = 2.0 * random.random() - 1.0
-    return x, y
+    return (x,y)
 
 if __name__ == "__main__":
     main()
